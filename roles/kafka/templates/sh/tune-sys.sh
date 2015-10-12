6ac32628ffe8e29d3f1f808456fe70a747a930a2
patoch <pguillebert@gmail.com>
2015-10-13 00:18:30 +0200

    Added DSE 4.8.0 support. Addition of Kafka ongoing.

---
+++ roles/kafka/templates/sh/tune-sys.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo deadline > /sys/block/vda/queue/scheduler
+echo 0 > /proc/sys/vm/zone_reclaim_mode
+
+# readadhead to 64 
+blockdev --setra 64 /dev/vda1
+
+# swap off
+echo  0 > /proc/sys/vm/swappiness
+swapoff --all
+
+
+# user limits
+grep -q -F '* - nproc 32768' /etc/security/limits.d/90-nproc.conf || echo '* - nproc 32768' >> /etc/security/limits.d/90-nproc.conf
+grep -q -F 'vm.max_map_count = 131072' /etc/sysctl.conf || echo 'vm.max_map_count = 131072' >> /etc/sysctl.conf
